f0bff28d1438a119181a948d3cd31bda2bf55530
thomasroodson <thomas.roodson@live.com>
2023-01-13 17:30:27 -0300

    Grid Item

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import logoImage from './assets/devmemory_logo.png'
 import RestartIcon from './svgs/restart.svg'
 import { Button } from './components/Button'
 import { InfoIntem } from './components/InfoItem'
+import { GridItem } from './components/GridItem'
 import { GridItemType } from './types/GridItemType'
+import { items } from './data/items'
 
 const App = () => {
   const [playing, setPlaying] = useState<boolean>(false)
@@ -15,10 +17,36 @@ const App = () => {
   useEffect(() => resetAndCreateGrid(), [])
 
   const resetAndCreateGrid = () => {
+    // Resetar o jogo
+    setTimeElapsed(0)
+    setMoveCount(0)
+    setShowCount(0)
 
+    // criar o grid
+    // Grid vázio
+    let tempGrid: GridItemType[] = []
+    for (let i = 0; i < (items.length * 2); i++){
+      tempGrid.push({item: null, shown: false, permanentShown: false})
+    }
+
+    //preencher o grid
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1
+        while(pos < 0 || tempGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length *2))
+        }
+        tempGrid[pos].item = i
+      }
+    }
+
+    //jogar no state
+    setGridItems(tempGrid)
+    
+    //começar o jogo
+    setPlaying(true)
 
   }
-
 
   return (
     <C.Container>
@@ -35,7 +63,11 @@ const App = () => {
         <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid}/>
       </C.Info>
       <C.GridArea>
-        <C.Grid></C.Grid>
+        <C.Grid>
+          {gridItems.map((item, index) => (
+            <GridItem key={index} />
+          ))}
+        </C.Grid>
       </C.GridArea>
     </C.Container>
   )
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,6 +1,6 @@
 import android from '../svgs/android.svg'
 import caminhao from '../svgs/caminhao.svg'
-import disney from '../svgs/diney.svg'
+import disney from '../svgs/disney.svg'
 import estrela from '../svgs/estrela.svg'
 import gasolina from '../svgs/gasolina.svg'
 import moto from '../svgs/moto.svg'
